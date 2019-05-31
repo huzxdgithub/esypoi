ca38a204e27a6dd14847659aee13707650f57cad
huzx <huzxdyx@126.com>
2019-05-31 13:07:50 +0800

    this is init

---
+++ src/test/java/com/baizhi/esypoi/EsypoiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baizhi.esypoi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EsypoiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
