2d1382d1c37956fb9b1e5ee7d59d6ae9e43f9d43
huzx <huzxdyx@126.com>
2019-05-31 13:49:27 +0800

    this is init

--- src/main/java/com/baizhi/EsypoiApplication.java
+++ src/main/java/com/baizhi/EsypoiApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EsypoiApplication {
     public static void main(String[] args) {
         SpringApplication.run(EsypoiApplication.class, args);
+        System.out.println();
     }
 }
